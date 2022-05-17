bd46a86c42abb7c71382b36db9e5faf8348bdbf1
Gabriel Manassés <gabrielmanasses13@gmail.com>
2022-05-17 05:31:24 -0300

    stage 05

--- main.js
+++ main.js
@@ -3,7 +3,36 @@ window.addEventListener('scroll', onScroll)
 onScroll()
 function onScroll() {
     showNavOnScroll()
-    showBackToTopButtonOnScroll()    
+    showBackToTopButtonOnScroll()
+    
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+    //activateMenuAtCurrentSection(services)
+}
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    // verificar se a seção passou da linha
+    // quais dados vou precisar?
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+    
+    menuElement.classList.remove('active')
+    if (sectionBoundaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
